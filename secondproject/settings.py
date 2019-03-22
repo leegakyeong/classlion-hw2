631ba80a398a4ec2164d5ae18022d2a351100ae0
unknown <alaska7132@naver.com>
2019-03-22 22:08:34 +0900

    에러 해결

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xc%)$*r$s1zzeu#g-pev#9q%v6drfp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
